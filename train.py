1a1d69b707cff8347909b837bebd3f1b1de6dbfd
MB-29 <matthieu.blanke@laposte.net>
2020-12-30 18:55:43 +0100

    traaaaaain

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ image_size = 28
 n_channels = 1
 
 batch_size = 1
-n_epoch = 5
+n_epochs = 20
 input_dim = image_size * image_size * n_channels
 embedding_dim = 64
 learning_rate = 1e-3
@@ -39,46 +39,37 @@ test_loader = torch.utils.data.DataLoader(
                                ])),
     batch_size=batch_size, shuffle=True)
 
-batch = next(iter(train_loader))
 
-# print('model')
-model = VariationalAutoEncoder(input_dim, embedding_dim)
-print('optimizer')
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+def train(model, optimizer, n_epochs):
+    training_loss_values = []
+    print(f'Training loop')
 
+    for epoch_index in range(n_epochs):
+        print(f'Epoch {epoch_index}')
+        epoch_loss = 0
 
-# Train
-training_losses = []
-print(f'Training loop')
+        for batch_index, (image_input, label) in enumerate(train_loader):
+            if batch_index > 5000:
+                break
+            if batch_index % 100 == 0:
+                print(f'Batch index {batch_index}')
 
-for epoch_index in range(n_epoch):
-    print(f'Epoch {epoch_index}')
-    epoch_loss = 0
+            input_vector = image_input.view(batch_size, input_dim)
+            generated_sample, embedded_mean, embedded_log_var = model(input_vector)
 
-    for batch_index, (image_input, label) in enumerate(train_loader):
-        if label != 5:
-            continue
-        if batch_index > 1000:
-            break
-        if batch_index % 100 == 0:
-            print(f'Batch index {batch_index}')
+            loss = model.loss(input_vector, generated_sample,
+                            embedded_mean, embedded_log_var)
 
-        input_vector = image_input.view(batch_size, input_dim)
-        generated_sample, embedded_mean, embedded_log_var = model(input_vector)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        loss = model.loss(input_vector, generated_sample,
-                          embedded_mean, embedded_log_var)
+            epoch_loss += loss
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        training_loss_values.append(epoch_loss)
+    return training_loss_values
 
-        epoch_loss += loss
-
-    training_losses.append(epoch_loss)
-
-
-def generate_image():
+def generate_image(model):
     embedding_sample = model.sample(torch.zeros(embedding_dim),
                           torch.ones(embedding_dim))
     generated_sample = model.decode(embedding_sample)
@@ -87,12 +78,50 @@ def generate_image():
 def vector_to_image(vector):
     return vector.reshape((image_size, image_size, n_channels))
 
+def plot_generated_samples(model, n_samples=5):
+    for sample_index in range(1, n_samples+1):
+        ax = plt.subplot(1, n_samples, sample_index)
+        ax.imshow(generate_image(model), cmap='gray')
+        plt.axis('off')
 
-T = 10000
-x = batch[0].view(batch_size, input_dim)
 
-sampler = Sampler(input_dim, embedding_dim, model, x, T)
-sampled_output = vector_to_image(sampler.sample_gibbs().data)
+def plot_images(samples):
+    for sample_index, sample in enumerate(samples):
+        ax = plt.subplot(1, len(samples), sample_index+1)
+        ax.imshow(sample, cmap='gray')
+        plt.axis('off')
 
-plt.imshow(sampled_output, cmap='gray')
-plt.show()
+
+batch = next(iter(train_loader))
+
+# TRAIN
+model = VariationalAutoEncoder(input_dim, embedding_dim)
+print('optimizer')
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+print(n_epochs)
+training_loss_values = train(model, optimizer, n_epochs)
+
+# RECONSTRUCTION
+
+def reconstruct(model, x0, missing_data_indices, T, method='pseudo_Gibbs'):
+    sampler = Sampler(model, x0.clone(), missing_data_indices, T)
+    sampling_method = sampler.sample_pseudo_gibbs if method=='pseudo_Gibbs' else sampler.sample_metropolis
+    sampled_vectors = sampling_method()
+    sampled_images = np.array([vector_to_image(vector)
+                               for vector in sampled_vectors])
+    error = (x0 - torch.Tensor(sampled_vectors)).norm(dim=1)
+    return sampled_images, error
+
+
+T = 100
+t_sample_values = np.linspace(0, T-1, num=10, dtype=int)
+missing_data_indices = np.arange(200, 500)
+
+x = batch[0][0].view(input_dim)
+x0 = x.data.clone()
+
+# add noise
+x.data[missing_data_indices] = torch.abs(torch.randn(size=(len(missing_data_indices),)))
+
+# output
+sampled_images, error = reconstruct(model, x0, missing_data_indices, T)
